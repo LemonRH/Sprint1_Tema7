2ed0cac12475389d109e19583f34e160f3d2fb99
LemonRH <82542162+LemonRH@users.noreply.github.com>
2023-11-08 09:45:50 +0100

    Update notasTest.php
    resolucion del ejercicio para un mejor funcionamiento

--- Ejercicio2/notasTest.php
+++ Ejercicio2/notasTest.php
@@ -4,42 +4,34 @@ use PHPUnit\Framework\TestCase;
 
 require_once 'notas.php';
 
-class  NotasTest extends TestCase{
-    public function testPrimeraDivision(){
-        $this->expectOutputString("Primera división\n");
-        $this->setInpuStream("5\n");
+use PHPUnit\Framework\TestCase;
+
+class CalculadoraNotasTest extends TestCase {
+    public function testPrimeraDivision() {
+        $calculadora = new CalculadoraNotas();
+        $this->assertEquals("Primera division", $calculadora->calcularDivision(8));
     }
+
     public function testSegundaDivision() {
-        $this->expectOutputString("Segunda división\n");
-        $this->setInpuStream("5\n");
-        notas();
+        $calculadora = new CalculadoraNotas();
+        $this->assertEquals("Segunda division", $calculadora->calcularDivision(5));
     }
 
     public function testTerceraDivision() {
-        $this->expectOutputString("Tercera division\n");
-        $this->setInpuStream("3.5\n");
-        notas();
+        $calculadora = new CalculadoraNotas();
+        $this->assertEquals("Tercera division", $calculadora->calcularDivision(3.5));
     }
 
     public function testSuspenso() {
-        $this->expectOutputString("SUSPENSO\n");
-        $this->setInpuStream("2.5\n");
-        notas();
+        $calculadora = new CalculadoraNotas();
+        $this->assertEquals("SUSPENSO", $calculadora->calcularDivision(2.5));
     }
 
-    public function testEntradaInvalida() {
-        $this->expectOutputString("No has introducido un numero correcto.\n");
-        $this->setInpuStream("texto\n");
-        notas();
+    public function testNotaIncorrecta() {
+        $calculadora = new CalculadoraNotas();
+        $this->assertEquals("No has introducido un numero correcto.", $calculadora->calcularDivision(-1));
     }
-    private function setInpuStream($input) {//probar diferentes entradas para la ejecucion de pruebas
-        $stream = fopen('php://memory', 'r+');
-        fwrite($stream, $input);
-        rewind($stream);
-        stream_set_blocking(STDIN, 0);
-        stream_set_blocking($stream, 0);
-        stream_copy_to_stream($stream, STDIN);
-
 }
+//cambiamos un poco la logica de los test para que sean mas simples 
 
 ?>
